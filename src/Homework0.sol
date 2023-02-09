ee9536315f848bb266d56320eac44366889d0c1d
lndavis6 <lancendavis47@gmail.com>
2023-02-09 13:02:58 -0500

    Homework0 done

---
+++ src/Homework0.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Homework0 {
+    // Define a public 256-bit unsigned integer called "payment" and assign it with the value of 1 ether
+    
+
+    // do not edit this function
+    function getPayment() public view returns (uint256) {
+        return payment;
+    }
+}
---
+++ test/Homework0.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Homework0.sol";
+
+contract CounterTest is Test {
+    Homework0 homework0;
+
+    function setUp() public {
+        homework0 = new Homework0();
+    }
+
+    function testNumber() public {
+        uint256 testAmount = 1 ether;
+        assertEq(homework0.getPayment(), testAmount);
+    }
+}
