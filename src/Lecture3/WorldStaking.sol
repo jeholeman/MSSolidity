0da54b1d042a1fcd95e212dc32b0d6715d7af488
lancenonce <lancendavis47@gmail.com>
2023-05-08 16:57:33 -0400

    more fixes

--- src/Lecture3/WorldStaking.sol
+++ src/Lecture3/WorldStaking.sol
@@ -7,14 +7,18 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // The WorldToken contract inherits the ERC20 contract
 contract WorldStaking {
     // we can interact with other contracts by declaring them as state variables
-    WorldToken worldToken;
-    address deadaddress = address(0xdeadbeef);
+    WorldToken public worldToken;
+
+    // initialize with constructor
+    constructor(WorldToken _worldToken) {
+        worldToken = _worldToken;
+    }
+    // address deadaddress = address(0xdeadbeef);
 
     // warning: none of this code works. just for proof of concept
     function stake(uint256 amount) public {
         worldToken.faucet(amount);
         worldToken.approve(msg.sender, amount);
         worldToken.transferFrom(msg.sender, address(this), amount);
-        IERC20(deadaddress).transfer(msg.sender, amount);
     }
 }
