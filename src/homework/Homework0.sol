34c979d0d8fec0373f1bda0ca96506335163100a
Jordan Holeman <jholeman2001@gmail.com>
2023-05-19 13:19:15 -0400

    homework 0 solution

--- src/homework/Homework0.sol
+++ src/homework/Homework0.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.13;
 
 contract Homework0 {
     // Define a public 256-bit unsigned integer called "payment" and assign it with the value of 1 ether
-    
+    uint256 public payment;
+
+    constructor(){
+        payment = 1 ether;
+    }
 
     // do not edit this function
     // function getPayment() public view returns (uint256) {
